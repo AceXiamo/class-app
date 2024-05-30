12b18e3ea68f42f9eaf47aef5f26c83f2cfc7aab
15767833949 <tech@vapinzhi.com>
2024-05-30 18:38:58 +0800

    修改活动/资讯页面

--- src/pages/activity/index.vue
+++ src/pages/activity/index.vue
@@ -1,73 +1,143 @@
 <template>
   <view class="h-full text-sm overflow-hidden">
-    <view class="h-6 flex justify-between my-4">
-      <view @tap="check(index)" v-for="(item, index) in options"
-        :class="{ 'font-bold': index == option, 'border-r-black border-solid border-r-[1rpx]': index == 0 }"
-        :key="index" class="text-base flex items-center justify-center w-full">{{ item }}</view>
+    <view class="h-[60rpx] flex justify-between py-[20rpx] bg-white">
+      <view
+        @tap="check(index)"
+        v-for="(item, index) in options"
+        :class="{
+          'text-[#92003F]': index === option,
+          'text-black': index !== option,
+          'border-0 border-r-[#F8F8F8] border-solid border-r-[1rpx]': index == 0,
+        }"
+        :key="index"
+        class="text-base flex items-center justify-center w-full font-medium relative"
+      >
+        <image
+          v-show="0 === option && index === 0"
+          src="/static/images/icon-event-selected.png"
+          class="w-4.5 h-4.5 mr-[10rpx]"
+        />
+        <image
+          v-show="1 === option && index === 0"
+          src="/static/images/icon-event.png"
+          class="w-4.5 h-4.5 mr-[10rpx]"
+        />
+        <image
+          v-show="1 === option && index === 1"
+          src="/static/images/icon-news-selected.png"
+          class="w-4.5 h-4.5 mr-[10rpx]"
+        />
+        <image
+          v-show="0 === option && index === 1"
+          src="/static/images/icon-news.png"
+          class="w-4.5 h-4.5 mr-[10rpx]"
+        />
+        {{ item }}
+        <view
+          v-if="index === option"
+          class="bg-[#92003F] w-[124rpx] h-0.75 absolute bottom-[-20rpx]"
+        ></view>
+      </view>
     </view>
 
     <!-- 组织活动 -->
-    <view class="flex justify-center" style="height: calc(100% - 112rpx)">
+    <view class="flex justify-center" style="height: calc(100% - 100rpx)">
       <od-list class="w-full" :total="total" :load-more="more" @on-load-more="lowerBottom">
-        <view v-if="option == 0" v-for="(item, index) in  list " :key="index" @tap="toEvent(item.id)"
-          class="h-44 flex items-center px-2 py-4 my-4 mx-2 border-solid border-repeat-79 border-[1rpx]">
-          <view class="h-full text-xs flex-1 px-1 flex flex-col">
-            <view class="text-base font-bold break-all line-clamp-2 h-12">{{ item?.theme }}</view>
-            <view class="mt-4">活动时间：{{ item?.eventTime }}</view>
-            <view class="flex mt-4">
-              <view class="w-[152rpx]">活动地点：</view>
-              <view class="break-all line-clamp-2 w-full">{{ item?.eventLocation }}
+        <view
+          v-if="option == 0"
+          v-for="(item, index) in [
+            {
+              id: 1,
+              theme: 'test',
+              eventTime: '2023-03-01',
+              eventLocation: 'test',
+              mainImg: '/static/images/icon-event.png',
+              isEnroll: true,
+            },
+          ]"
+          :key="index"
+          @tap="toEvent(item.id)"
+          class="flex items-center px-[28rpx] pt-[30rpx] pb-[34rpx] m-[30rpx] rounded-[16rpx] bg-white text-repeat-33"
+        >
+          <view class="h-full text-[26rpx] flex-1 flex flex-col">
+            <view
+              class="font-medium break-all line-clamp-2 text-[30rpx] h-[92rpx] flex items-center text-black"
+              >{{ item?.theme }}</view
+            >
+            <view class="mt-1 flex items-center h-5.5">活动时间：{{ item?.eventTime }}</view>
+            <view class="break-all line-clamp-1 flex w-full">
+              <view class="w-[190rpx]">活动地点：</view>
+              <view class="break-all line-clamp-2 w-full h-[78rpx]">
+                {{ item?.eventLocation }} 11111111111111111111111111111111111111111111111
               </view>
             </view>
           </view>
-          <view class="ml-2 flex flex-col items-end">
-            <image mode="aspectFill" class="w-25 h-25 border-solid border-repeat-79 border-[1rpx]" :src="item?.mainImg">
+          <view class="ml-4 flex flex-col items-end">
+            <image
+              mode="aspectFill"
+              class="w-[172rpx] h-[172rpx] rounded-[8rpx]"
+              :src="item?.mainImg"
+            >
             </image>
-            <view class="mt-4 text-center w-16 rounded-md p-1 border-[1rpx] border-solid border-repeat-79"
-              :class="item?.isEnroll ? 'bg-repeat-f7' : ''">
-              {{ item?.isEnroll ? '已报名' : '报名' }}</view>
+            <view
+              class="mt-4 text-center w-16 rounded-md p-1 border-[1rpx] border-solid border-repeat-79"
+              :class="item?.isEnroll ? 'bg-repeat-f7' : ''"
+            >
+              {{ item?.isEnroll ? '已报名' : '报名' }}</view
+            >
           </view>
         </view>
 
-        <view v-if="option == 1" v-for="( item, index ) in  list " :key="index" @tap="toNews(item.id)"
-          class="h-36 flex items-center px-2 py-4 my-4 mx-2 border-solid border-repeat-79 border-[1rpx]">
-          <view class="h-full text-xs flex-1 px-1 flex flex-col justify-between">
-            <view class="text-base font-bold break-all line-clamp-2">{{ item?.title }}</view>
-            <view class="mt-2">发布时间：{{ item?.publishTime }}</view>
-            <view>来源：{{ item?.origin }}</view>
+        <view
+          v-if="option == 1"
+          v-for="(item, index) in list"
+          :key="index"
+          @tap="toNews(item.id)"
+          class="flex items-center px-[28rpx] pt-[30rpx] pb-[34rpx] m-[30rpx] rounded-[16rpx] bg-white text-repeat-33"
+        >
+          <view class="h-full text-[26rpx] flex-1 flex flex-col">
+            <view
+              class="font-medium break-all line-clamp-2 text-[30rpx] h-[92rpx] flex items-center text-black"
+              >{{ item?.title }}</view
+            >
+            <view class="mt-1 flex items-center h-5.5">发布时间：{{ item?.publishTime }}</view>
+            <view class="break-all line-clamp-1 h-5.5">来源：{{ item?.origin }}</view>
           </view>
-          <view class="ml-2">
-            <image mode="aspectFill" class="w-25 h-25 border-solid border-repeat-79 border-[1rpx]" :src="item?.image">
+          <view class="ml-4">
+            <image
+              mode="aspectFill"
+              class="w-[172rpx] h-[172rpx] rounded-[8rpx]"
+              :src="item?.image"
+            >
             </image>
           </view>
         </view>
         <template #empty>
-          <view class="pb-[50rpx] text-xs flex items-center justify-center text-repeat-aa">
+          <view class="pt-4 pb-[50rpx] text-xs flex items-center justify-center text-repeat-aa">
             没有更多了
           </view>
         </template>
       </od-list>
     </view>
-
   </view>
 </template>
 
 <script lang="ts"></script>
 
 <script lang="ts" setup>
-import OdList from '@/components/list/index.vue';
-import { computed, ref } from 'vue';
+import OdList from '@/components/list/index.vue'
+import { computed, ref } from 'vue'
 import * as event from '@/api/app/event'
 import { page_23 } from '@/api/app/news'
-import { onLoad, onShow } from '@dcloudio/uni-app';
+import { onLoad, onShow } from '@dcloudio/uni-app'
 
 let page = ref(1)
 let list: any = ref([])
 let recordTotal = ref(0)
 let total = ref(0)
 const more = computed(() => {
-  return list.value.length < recordTotal.value;
-});
+  return list.value.length < recordTotal.value
+})
 
 const toEvent = (id: number) => {
   uni.navigateTo({ url: `/pages/activity/event/index?id=${id}` })
@@ -84,7 +154,7 @@ const lowerBottom = () => {
 
 const loadData = () => {
   if (option.value == 0) {
-    event.page_24({ page: page.value, limit: 10 }).then(res => {
+    event.page_24({ page: page.value, limit: 10 }).then((res) => {
       recordTotal.value = res.data?.total
       if (res.data?.list) {
         list.value = list.value.concat(res.data?.list)
@@ -95,7 +165,7 @@ const loadData = () => {
       }
     })
   } else {
-    page_23({ page: page.value, limit: 10 }).then(res => {
+    page_23({ page: page.value, limit: 10 }).then((res) => {
       recordTotal.value = res.data?.total
       if (res.data?.list) {
         list.value = list.value.concat(res.data?.list)
@@ -108,10 +178,10 @@ const loadData = () => {
   }
 }
 
-let option = ref(0);
-let options = ['活动报名', '组织资讯'];
+let option = ref(0)
+let options = ['活动报名', '组织资讯']
 const check = (index: number) => {
-  option.value = index;
+  option.value = index
   page.value = 1
   list.value = []
   recordTotal.value = 0
@@ -126,9 +196,6 @@ onShow(() => {
   total.value = 0
   loadData()
 })
-
-
-
 </script>
 
 <style scoped></style>
